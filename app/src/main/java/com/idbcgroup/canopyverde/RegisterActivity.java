3a8eaf24de9321c4cd306224b3795f0caa0e9b91
sahidr <sahidr93@gmail.com>
2017-06-19 21:07:36 -0400

    Fixed Spinner color text

--- app/src/main/java/com/idbcgroup/canopyverde/RegisterActivity.java
+++ app/src/main/java/com/idbcgroup/canopyverde/RegisterActivity.java
@@ -2,9 +2,11 @@ package com.idbcgroup.canopyverde;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,9 +16,11 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
@@ -35,8 +39,50 @@ public class RegisterActivity extends AppCompatActivity {
                 .build()
         );
 
-
         country = (Spinner) findViewById(R.id.country);
+        city = (Spinner) findViewById(R.id.city);
+
+/*
+        ArrayAdapter<String> countryAdapter = new ArrayAdapter<String>(RegisterActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.country_array));
+
+        countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        country.setAdapter(countryAdapter)
+*/
+
+        country.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                if (position!=0){
+                    ((TextView) parent.getChildAt(0)).setTextColor(getResources().getColor(R.color.fontColor));
+                    // Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" Selected", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        city.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                if (position!=0){
+                    ((TextView) parent.getChildAt(0)).setTextColor(getResources().getColor(R.color.fontColor));
+                    // Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" Selected", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
     }
 
@@ -46,11 +92,11 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void toLogin(View view){
-        Intent i = new Intent(RegisterActivity.this,LoginActivity.class);
+
+        Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
         i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(i);
         finish();
-
     }
 
 
